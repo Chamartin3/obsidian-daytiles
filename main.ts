98439d915a1e25970d36622e86417c1b57a25fa4
Omar Espana <omi.fullstack@gmail.com>
2025-05-02 12:53:54 +0200

    load and save settings

--- main.ts
+++ main.ts
@@ -1,12 +1,31 @@
 import { Plugin } from "obsidian";
 import { registerCodeBlock } from "./src/codeBlockProcessor";
+import { DEFAULT_SETTINGS, type DaytilesPluginSettings } from "./src/settings";
+import { DaytilesSettingTab } from "./src/settingsTab";
 
 export default class DaytilesPlugin extends Plugin {
+  settings: DaytilesPluginSettings = DEFAULT_SETTINGS;
+
   async onload(): Promise<void> {
+    await this.loadSettings();
+    this.addSettingTab(new DaytilesSettingTab(this.app, this));
     registerCodeBlock(this);
   }
 
   async onunload(): Promise<void> {
     /* no-op: registrations are torn down by Obsidian */
+  }
+
+  async loadSettings(): Promise<void> {
+    const data = (await this.loadData()) as Partial<DaytilesPluginSettings> | null;
+    this.settings = {
+      ...DEFAULT_SETTINGS,
+      ...(data ?? {}),
+      defaults: { ...DEFAULT_SETTINGS.defaults, ...(data?.defaults ?? {}) }
+    };
+  }
+
+  async saveSettings(): Promise<void> {
+    await this.saveData(this.settings);
   }
 }
